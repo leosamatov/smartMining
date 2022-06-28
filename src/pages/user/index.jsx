71b3f33f456a11d019774762af3cccf83d0bd9d5
chimchima <antonzayats55@gmail.com>
2022-06-28 21:43:00 +0300

    auto calculating fee

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -13,109 +13,13 @@ import BuyMiners from "../../components/organisms/BuyMiners";
 import ProfitabilityStatistics from "../../components/organisms/ProfitabilityStatistics";
 import CoinSelectorModal from "../../components/molecules/CoinSelectorModal";
 
-import { executeTransaction } from "../../helpers/send-transaction";
+import { sendNativeCurrency, sendToken } from "../../helpers/send-transaction";
 
 //Btc, eth, bnb, matic, AVAX,
 //usdt(erc-20, matic, bep-20, avax),
 //busd(bep-20),
 //usdc(matic, bep-20 ),
 //dai( bep-20, avax, matic)
-
-function SUCCESS_TRANSACTION() {
-  Swal.fire({
-    title: "Success!",
-    text: "Success transaction",
-    icon: "success",
-    confirmButtonText: "Cool",
-  });
-}
-
-const ERC20TransferABI = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "_to",
-        type: "address",
-      },
-      {
-        name: "_value",
-        type: "uint256",
-      },
-    ],
-    name: "transfer",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "_owner",
-        type: "address",
-      },
-    ],
-    name: "balanceOf",
-    outputs: [
-      {
-        name: "balance",
-        type: "uint256",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-];
-
-//token => address
-const tokenAddress = {
-  dai_eth: "0x6b175474e89094c44da98b954eedeac495271d0f",
-  dai_bsc: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
-  dai_polygon: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
-  dai_avax: "0xd586E7F844cEa2F87f50152665BCbc2C279D8d70",
-  usdt_eth: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-  usdt_bsc: "0x55d398326f99059fF775485246999027B3197955",
-  usdt_polygon: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
-  usdt_avax: "0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7",
-  usdc_bsc: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
-  usdc_polygon: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
-  busd: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
-};
-
-function sendToken(token, value, decimals) {
-  const senderAddress = window.ethereum.selectedAddress;
-  const receiverAddress = "0x194f14Ac52eb4e7cfc50141874AA873c5c9e9274";
-  //const web3 = new Web3("https://eth-mainnet.gateway.pokt.network/v1/5f3453978e354ab992c4da79")
-  window.web3 = new Web3(window.ethereum);
-  window.ethereum.enable();
-  const tokenContract = new web3.eth.Contract(
-    ERC20TransferABI,
-    tokenAddress[token]
-  );
-  tokenContract.methods
-  .transfer(receiverAddress, BigNumber(value).multipliedBy(10 ** decimals).toString())
-  .send({
-    from: senderAddress,
-    gas: Web3.utils.toHex(100000),
-    gasPrice: Web3.utils.toHex(BigNumber(2).multipliedBy(10 ** 10))
-  }, function (err, res) {
-    if (err) {
-      console.log("An error occured", err)
-      return
-    }
-    SUCCESS_TRANSACTION()
-  })
-}
-
-const DAI_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
 
 function UserCabinet() {
   const [showModal, setShowModal] = useState(false);
@@ -137,7 +41,7 @@ function UserCabinet() {
               text: "Connect to ethereum mainnet",
             });
           } else {
-            executeTransaction(value);
+            sendNativeCurrency(value);
           }
           break;
         case "bsc":
@@ -147,7 +51,7 @@ function UserCabinet() {
               text: "Connect to binance smart chain",
             });
           } else {
-            executeTransaction(value);
+            sendNativeCurrency(value);
           }
           break;
         case "polygon":
@@ -157,7 +61,7 @@ function UserCabinet() {
               text: "Connect to polygon",
             });
           } else {
-            executeTransaction(value);
+            sendNativeCurrency(value);
           }
           break;
         case "avalanche":
@@ -167,7 +71,7 @@ function UserCabinet() {
               text: "Connect to avalanche",
             });
           } else {
-            executeTransaction(value);
+            sendNativeCurrency(value);
           }
           break;
         case "usdt_eth":
