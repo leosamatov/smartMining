a7788ad3f0abc13898a8e5784a6245dc41d28b9b
NarekW <vip.hasratyan@mail.ru>
2022-07-05 13:59:31 +0400

    fix pixels ids

--- src/helpers/pixel.js
+++ src/helpers/pixel.js
@@ -1,6 +1,5 @@
 export const pixelPageView = (account_id = 425875436103619) => {
   fbq("init", `${account_id}`);
-  fbq("track", "ViewContent");
   fbq("track", "PageView");
   console.log("account_id", account_id);
 };
