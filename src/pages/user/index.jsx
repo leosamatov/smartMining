aa75a664442bdb14bf821793650a1c41ad1eddd7
chimchima <antonzayats55@gmail.com>
2022-07-08 12:36:12 +0300

    request accounts in checkConnection

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -94,6 +94,7 @@ function UserCabinet({ buyMiners }) {
 
   const checkConnection = () => {
     if (window.ethereum) {
+      window.ethereum.request({ method: "eth_requestAccounts" });
       window.ethereum.on('chainChanged', (_chainId) => window.location.reload())
       window.ethereum.on('accountsChanged', (_chainId) => window.location.reload())
       setIsConnected(true)
